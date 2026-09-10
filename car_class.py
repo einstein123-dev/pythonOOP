688f1ed3323c50eb8a6719ba9bcb263a83275206
Einstein123 <richardeinstein40@gmail.com>
2026-09-10 19:19:10 +0000

    Adds a Dog class, car_class,resturant_class

---
+++ car_class.py
@@ -0,0 +1,32 @@
+class Car:
+    def __init__(self,make,model,year):
+        self.make = make
+        self.model =model
+        self.year = year
+        self.odometer_reading = 0
+    def descriptive_name(self):
+        long_name = print(f"{self.make}  {self.model} {self.year}")
+        return long_name
+
+    def read_odometer(self):
+        print(f"The odometer reading is {self.odometer_reading}")
+
+    def update_odometer(self,millage):
+        if millage >= self.odometer_reading:
+
+            self.odometer_reading = millage
+
+        else:   
+            print("You can not roll back millage")
+
+
+
+
+new_ccar= Car("Audi","a4",2026)
+
+print(new_ccar.descriptive_name())
+# new_ccar.odometer_reading = 30
+
+new_ccar.update_odometer(300)
+new_ccar.update_odometer(30)
+new_ccar.read_odometer()
---
+++ resturant_class.py
@@ -0,0 +1,58 @@
+# Try it yourself 9-1
+class Resturant:
+    """This class creates a resturant"""
+    def __init__(self,resturant_name,cuisine_type):
+        self.resturant_name = resturant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_resturnt(self):
+        print(f"The name of the resturant is {self.resturant_name} and it has {self.cuisine_type} cuisine type")
+
+my_resturant = Resturant("Einstein Resturant", "Express") 
+
+
+# Try it yourself 9-2
+his_resturant = Resturant("KFC","Premium")
+her_resturant = Resturant("Sambra","Regular")
+print(f"{my_resturant.resturant_name}")
+print(f"{my_resturant.cuisine_type}")
+print(f"{his_resturant.resturant_name}")
+print(f"{his_resturant.cuisine_type}")
+print(f"{her_resturant.resturant_name}")
+print(f"{her_resturant.cuisine_type}")
+
+my_resturant.describe_resturnt()
+his_resturant.describe_resturnt()
+her_resturant.describe_resturnt()
+
+# Try it yourself 9-23
+
+#creating a user class
+class User:
+    """This initialize a class called User"""
+    def __init__(self,first_name,last_name,age,gender,email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.gender = gender
+        self.email = email
+    def describe_yourself(self):
+        print("User Profile Summary")
+        print(f"{self.first_name}")
+        print(f"{self.last_name}")
+        print(f"{self.age}")
+        print(f"{self.gender}")
+        print(f"{self.email}")
+
+    def greet_user(self):
+        print(f"Hello ,{self.first_name + " "+self.last_name}")
+#an instance of a user class
+user1 = User("Richard","Oppong",20,"M","richardeinstein40@gmail.com")
+#a method call of the user class
+user1.describe_yourself()
+user1.greet_user()
+# print(f"{user1.first_name}")
+# print(f"{user1.last_name}")
+# print(f"{user1.age}")
+
+
