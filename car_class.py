47ccf5d025e50b149466c0a7e38c271aae021eae
Einstein123 <richardeinstein40@gmail.com>
2026-09-12 23:08:02 +0000

    Add ineritance concepts and it usage

--- car_class.py
+++ car_class.py
@@ -22,14 +22,23 @@ class Car:
     def increment_odometer(self,miles):
         self.odometer_reading += miles
 
+class ElectricCar(Car):
+    def __init__(self,make,model,year):
+        super().__init__(make,model,year)
 
-
+        self.battery_size =40
+    def describe_battery(self):
+        print(f"{self.battery_size} watts is the current size of te battery")
 
 
 new_ccar= Car("Audi","a4",2026)
 
 print(new_ccar.descriptive_name())
 # new_ccar.odometer_reading = 30
+my_leaf = ElectricCar("nissin","leaf",2024)
+
+print(my_leaf.descriptive_name())
+my_leaf.describe_battery()
 
 new_ccar.update_odometer(300)
 new_ccar.update_odometer(30)
